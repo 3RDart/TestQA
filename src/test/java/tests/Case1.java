bf098b1f53b5efeaa2c73e68eb1386af9a4dfe66
Viktor_Goldov <Goldov_V@mail.ru>
2021-05-20 21:11:03 +0400

    Создание 1 теста- попытка вписать в калькуляторо цифры2+на полный экран

--- src/test/java/tests/Case1.java
+++ src/test/java/tests/Case1.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 //Тест 1. Проверка операций с целыми числами.
 public class Case1 {
     @Test
     public void test1(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
         driver.findElement(By.cssSelector ("input.gLFyf.gsfi")).sendKeys("калькулятор", Keys.ENTER);
