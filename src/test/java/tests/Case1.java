aca264036334d02299f07bc792c874d7e24a73d3
Viktor_Goldov <Goldov_V@mail.ru>
2021-05-20 22:29:18 +0400

    Создание 1 теста- попытка вписать в калькуляторор Вынесли создание хрома на верх

--- src/test/java/tests/Case1.java
+++ src/test/java/tests/Case1.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -9,15 +10,20 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 //Тест 1. Проверка операций с целыми числами.
 public class Case1 {
-    @Test
-    public void test1(){
+    private static WebDriver driver;
+    @BeforeAll
+    public static void init(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
+    }
+    @Test
+    public void test1(){
         driver.get("http://google.com");
         driver.findElement(By.cssSelector ("input.gLFyf.gsfi")).sendKeys("калькулятор", Keys.ENTER);
-        driver.findElement(By.cssSelector ("div.XRsWPe.AOvabd")).sendKeys("7", Keys.ENTER);
-        driver.quit();
+        //driver.findElement(By.cssSelector ("div.XRsWPe.AOvabd")).click();
+        driver.findElement(By.cssSelector ("div.abcgof")) .sendKeys(Keys.ENTER);
+        //driver.quit();
     }
 }
