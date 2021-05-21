b6b35f2525c966681e7cc27458caea19a92674f7
Viktor_Goldov <Goldov_V@mail.ru>
2021-05-21 21:29:51 +0400

    Создание 1 теста- Готовое задание без отдельного page класса.

--- src/test/java/tests/Case1.java
+++ src/test/java/tests/Case1.java
@@ -37,9 +37,8 @@ public class Case1 {
         driver.findElement (By.xpath("//div[@jsname='WxTTNd']")). click();
         driver.findElement (By.xpath("//div[@jsname='Ax5wH']")). click();
         driver.findElement (By.xpath("//div[@jsname='Pt8tGc']")). click();
-
     }
-//@AfterAll
+@AfterAll
 public static void fulldown() {
     driver.quit();
 }
