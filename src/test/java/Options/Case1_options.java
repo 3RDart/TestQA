b866f3f88da08634bd4ea7f7f00f0201064598b3
Viktor_Goldov <Goldov_V@mail.ru>
2021-05-21 22:12:49 +0400

    Создание 1 теста- Готовое задание Создание отдельного page класса.

--- src/test/java/Options/Case1_options.java
+++ src/test/java/Options/Case1_options.java
@@ -8,8 +8,54 @@ import org.openqa.selenium.support.FindBy;
 import static org.openqa.selenium.support.PageFactory.initElements;
 
 public class Case1_options {
-    @FindBy (css ="")
-    public WebElement Enter_numbers;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_1;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_2;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_3;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_4;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_5;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_6;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_7;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_8;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_9;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement num_0;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_dat;
+    //Символ =
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_eq;
+    //Символ процента
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_proc;
+    //Символ скобка (
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_9;
+    //Символ скобка )
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_0;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_CE;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_del;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_x;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_min;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement sym_plus;
+    @FindBy (xpath ="//div[@jsname='j93WEe']")
+    public WebElement fun_sin;
+
+
     public Case1_options(WebDriver driver) {
         initElements(driver, this);
     }
