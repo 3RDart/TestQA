442c5111efa837d42bfecdb0913edb47da733753
Viktor_Goldov <Goldov_V@mail.ru>
2021-05-21 20:55:40 +0400

    Создание 1 теста- удачная попытка внести цифры в калькулятор Ввел все выражения

--- src/test/java/tests/Case1.java
+++ src/test/java/tests/Case1.java
@@ -24,8 +24,18 @@ public class Case1 {
     public void case1(){
         driver.get("http://google.com");
         driver.findElement(By.cssSelector ("input.gLFyf.gsfi")).sendKeys("калькулятор", Keys.ENTER);
-        //driver.findElements(By.cssSelector ("div.XRsWPe.AOvabd. 2")). click();
+        driver.findElement (By.xpath("//div[@jsname='j93WEe']")). click();
+        driver.findElement (By.xpath("//div[@jsname='N10B9']")). click();
+        driver.findElement (By.xpath("//div[@jsname='XSr6wc']")). click();
+        driver.findElement (By.xpath("//div[@jsname='lVjWed']")). click();
+        driver.findElement (By.xpath("//div[@jsname='qCp9A']")). click();
+        driver.findElement (By.xpath("//div[@jsname='YovRWb']")). click();
+        driver.findElement (By.xpath("//div[@jsname='KN1kY']")). click();
+        driver.findElement (By.xpath("//div[@jsname='pPHzQc']")). click();
         driver.findElement (By.xpath("//div[@jsname='xAP7E']")). click();
+        driver.findElement (By.xpath("//div[@jsname='bkEvMb']")). click();
+        driver.findElement (By.xpath("//div[@jsname='WxTTNd']")). click();
+        driver.findElement (By.xpath("//div[@jsname='Ax5wH']")). click();
     }
 //@AfterAll
 public static void fulldown() {
